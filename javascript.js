7a648cc18235ce19b2cd6af17566f59f59d4b12d
Mentor-Ntech <oloyedeaderayo11@gmail.com>
2022-05-15 17:56:24 +0100

    init

--- javascript.js
+++ javascript.js
@@ -1,36 +1,69 @@
 'use strict'
-const atmCardBox = document.querySelector('.atmCardBox');
-const overlay = document.querySelector('.overlay');
-const flexBox = document.queryCommandValue('.flexBox');
-const btnFull = document.querySelector('.btnFull')
-const showPoint = document.querySelector('.showPoint')
-const holderName = document.querySelector('.holderName')
+const firstNameValue = document.getElementById('firstname');
+const lastNameValue = document.getElementById('lastname');
+const accountTypeValue = document.getElementById('account-type')
+const submitBtn = document.getElementById('submit');
 
-btnFull.addEventListener('click', () =>{
-    // console.log('click me')
-    if (atmCardBox !==1) {
+const accountTypeText = document.querySelector('.account-type');
+const cardDigit =document.querySelector('.cardDigit');
+const expiry = document.querySelector('.expiry-date');
+const firstNameText = document.querySelector('.firstname');
+const lastNameText = document.querySelector('.lastname');
 
-    }
+const atmProxy = document.querySelector('.atm-proxy');
+const atmCard = document.querySelector('.atmCardBox');
+
+// console.log(firstNameValue,lastNameValue,accountTypeValue,submitBtn)
+// console.log(firstNameText,cardDigit,expiry,lastNameText,accountTypeText)
+
+// FUNCTION
+const getCardDigit = () => {
+    const randomNum = Math.trunc(Math.random() * 10000000000000000) + '';
+    const lastFour = randomNum.slice(-4);
+    const padNum = lastFour.padStart(randomNum.length, '*');
+    return padNum;
+} 
+
+const genExpDate = () => {
+    const d = new Date();
+    const month = d.getMonth() +1
+    const fixedMonth = month < 10 ? '0' + month : month;
+    const year = (d.getFullYear() + 3) + '';
+    return `${fixedMonth}/${year.slice(-2)}`
+}
+
+
+const getFirstName = (nameStr) => {
+    const[firstXter, ...otherXter] = nameStr
+    return [firstXter.toUpperCase(), ...otherXter].join('')
+}
+const getLastName = (nameStr) => {
+    return nameStr.toUpperCase();
+}
+
+
+submitBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    console.log('submit button')
+
+    //Display Account Type
+accountTypeText.textContent = accountTypeValue.value
+
+  // Display Card number
+cardDigit.textContent = getCardDigit();
+
+  //Display Firstname
+firstNameText.textContent = getFirstName(firstNameValue.value)
+
+ // Display Lastname
+lastNameText.textContent = getLastName(lastNameValue.value)
+
+  //Display Expiry Date
+expiry.textContent = genExpDate();
+
+ //Display Atm card
+atmCard.classList.remove('hidden');
+atmProxy.classList.add('hidden')
+
+
 })
-
-function onWord (holderName) {
-    return holderName.replaceAll(' ', '').toLowerCase();
-}
-function capFirstWord(holderName) {
-    const [first, ...others] = holderName.split(' ');
-    return [first.toUpperCase(), ...others].join()
-}
-
-function transform (string, transformerFn) {
-    if (onWord === holderName) {
-        console.log(`Original sentence❤ ${string}`);
-    }else {
-        console.log(`Transformd sentence💨 ${transformerFn(string)}`);
-    }
-    // console.log(`Original sentence❤ ${string}`);
-    // console.log(`Transformd sentence💨 ${transformerFn(string)}`);
-    // console.log(`function name: ${transformerFn.name}`)
-
-}
-transform('naheem', onWord)
-transform('oloyede', capFirstWord)
